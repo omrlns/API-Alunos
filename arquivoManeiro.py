bbd0b860fd4c1af9fbebf2f8f40a1b765e0975cd
omrlns <marlon.da.silva996@gmail.com>
2025-07-05 22:03:41 -0300

    implementando a função de pesquisar um registro pelo nome com filter

--- arquivoManeiro.py
+++ arquivoManeiro.py
@@ -4,14 +4,15 @@ from datetime import datetime
 CSV_FILE = 'registros.csv'
 
 def limparTela():
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def exibirMenu():
-    print('=' * 15  + ' MENU ' + '=' * 15)
+    print('=' * 15 + ' MENU ' + '=' * 15)
     print('''
     [ 1 ] - CADASTRAR ALUNO
     [ 2 ] - LISTAR ALUNOS
-    [ 3 ] - SAIR
+    [ 3 ] - PESQUISAR ALUNO POR NOME
+    [ 4 ] - SAIR
     ''')
 
 def selecionarMenu():
@@ -21,18 +22,17 @@ def selecionarMenu():
     while True:
         try:
             opcao = int(input('ESCOLHA UMA OPÇÃO PARA PROSSEGUIR: '))
-            if (opcao >= 1 and opcao <= 3):
+            if (opcao >= 1 and opcao <= 4): # Faixa de opções atualizada para 1 a 4
                 return opcao
             else:
-                print('OPÇÃO INVÁLIDA! POR FAVOR, ESCOLHA 1, 2 OU 3.')
+                print('OPÇÃO INVÁLIDA! POR FAVOR, ESCOLHA UMA DAS OPÇÕES DO MENU.')
         except ValueError:
             print('ENTRADA INVÁLIDA! POR FAVOR, DIGITE UM NÚMERO.')
-
 
 def capturarDados():
     limparTela()
     
-    print('=' * 10  + ' FORMULÁRIO DE CADASTRO DE ALUNO ' + '=' * 10)
+    print('=' * 10 + ' FORMULÁRIO DE CADASTRO DE ALUNO ' + '=' * 10)
     nome = str(input('NOME: ')).strip()
     nascimento = str(input('DATA DE NASCIMENTO (DD/MM/AAAA): ')).strip()
     matricula = str(input('MATRÍCULA: ')).strip()
@@ -45,10 +45,9 @@ def capturarDados():
         'matricula': matricula,
         'email': email,
         'senha': senha
-        }
-    
+    }
     return dadosAlunos
-
+    
 def validarDados(dadosAlunos):
     erros = []
 
@@ -58,111 +57,122 @@ def validarDados(dadosAlunos):
     email = dadosAlunos['email']
     senha = dadosAlunos['senha']
 
-    # validação do nome
-    # verificando se a variável "nome" está vazia
-    if not nome: # .strip() remove os espaços em branco do início e fim
+    if not nome:
         erros.append('"NOME" NÃO PODE SER VAZIO!')
 
-    # validação da data de nascimento
     try:
-        valorNascimento = datetime.strptime(nascimento, '%d/%m/%Y') # '%d/%m/%Y' é o formato esperado para DD/MM/AAAA
-
+        valorNascimento = datetime.strptime(nascimento, '%d/%m/%Y')
         if (valorNascimento > datetime.now()):
             erros.append('"DATA DE NASCIMENTO" NÃO PODE SER NO FUTURO!')
-
     except ValueError:
         erros.append('FORMATO DE "DATA DE NASCIMENTO" INVÁLIDO! USE DD/MM/AAAA.')
 
-    # validação da matrícula
-    if not matricula.strip():
+    if not matricula:
         erros.append('"MATRÍCULA" NÃO PODE SER VAZIA!')
 
-    # validação do email
-    # regex para validação de e-mail com um padrão básico, como caracteres@caracteres.domínio
     emailRegex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     if not re.match(emailRegex, email):
         erros.append('FORMATO DE "E-MAIL" INVÁLIDO!')
 
-     # validação da senha
-    if not senha.strip():
+    if not senha:
         erros.append('"SENHA" NÃO PODE SER VAZIA!')
 
-    # retorno da validação
     if erros: 
-        return False, '\nDADOS INVÁLIDOS: \n' + '\n'.join(erros)
+        return False, '\nERROS NA VALIDAÇÃO DOS DADOS:\n' + '\n'.join(erros)
     else:
         return True, '\nDADOS VALIDADOS COM SUCESSO!'
-
-def carrgarAlunoCSV():
+    
+def carregarAlunosCSV():
     alunos = []
-    
     if not os.path.exists(CSV_FILE):
-        return alunos # se não existir, o sistema vai retornar uma lista vazia
-    
-    with open(CSV_FILE, mode = 'r', newline = '', encoding = 'utf-8') as file:
-        reader = csv.DictReader(file) #lê o csv como dicionários
+        return alunos
+
+    with open(CSV_FILE, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
         for row in reader:
             alunos.append(row)
     return alunos
 
 def salvarAlunoCSV(aluno):
-    # verifica se o arquivo já existe para saber se precisa escrever o cabeçalho
-    fileExists = os.path.exists(CSV_FILE)
-
-    with open(CSV_FILE, mode = 'a', newline = '', encoding = 'utf-8') as file:
-        # define os nomes dos campos (cabeçalhos) para o CSV
+    file_exists = os.path.exists(CSV_FILE)
+    
+    with open(CSV_FILE, mode='a', newline='', encoding='utf-8') as file:
         fieldnames = ['nome', 'nascimento', 'matricula', 'email', 'senha']
-        writer = csv.DictWriter(file, fieldnames = fieldnames)
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-        # se o arquivo não existe, escreve a linha do cabeçalho
-        if not fileExists:
+        if not file_exists:
             writer.writeheader()
 
-        writer.writerow(aluno) # escreve o dicionário do aluno como uma linha no CSV
+        writer.writerow(aluno)
 
 def cadastrar(dadosAlunos):
-    print('\n' + '=' * 10  + ' PROCESSO DE CADASTRO ' + '=' * 10)
+    print('\n' + '=' * 10 + ' PROCESSO DE CADASTRO ' + '=' * 10)
     print('NOME: {}'.format(dadosAlunos['nome']))
     print('DATA DE NASCIMENTO: {}'.format(dadosAlunos['nascimento']))
     print('MATRÍCULA: {}'.format(dadosAlunos['matricula']))
-    print('E-MAIL: {}'.format(dadosAlunos['email']))    
+    print('E-MAIL: {}'.format(dadosAlunos['email']))     
     print('SENHA: {}'.format(dadosAlunos['senha']))
 
-    alunosExistentes = carrgarAlunoCSV() # carrega os alunos existentes para verificar a duplicidade antes de salvar
+    alunosExistentes = carregarAlunosCSV()
 
-    # validação de duplicidade de matricula e email
-    for alunoExistente in alunosExistentes:
-        if alunoExistente['matricula'] == dadosAlunos['matricula']:
-            print('\nERRO: MATRÍCULA {} JÁ CADASTRADA!'.format(dadosAlunos['matricula']))
-            return False # retorna falso para indicar falha no cadastro
-    for alunoExistente in alunosExistentes:
-        if alunoExistente['email'] == dadosAlunos['email']:
-            print('\nERRO: E-MAIL {} JÁ CADASTRADO!'.format(dadosAlunos['email']))
-            return False # retorna falso para indicar falha no cadastro
-        
-    salvarAlunoCSV(dadosAlunos) # salva o aluno no CSV
-    print('\nCADASTRO REALIZADO COM SUCESSO!')
-    return True # retorna true para indicar sucesso no cadastro
+    for registroExistente in alunosExistentes:
+        if registroExistente['matricula'] == dadosAlunos['matricula']:
+            print(f'\nERRO: Matrícula {dadosAlunos["matricula"]} já cadastrada!')
+            return False
+
+    for registroExistente in alunosExistentes:
+        if registroExistente['email'] == dadosAlunos['email']:
+            print(f'\nERRO: E-mail {dadosAlunos["email"]} já cadastrado!')
+            return False
+
+    salvarAlunoCSV(dadosAlunos)
+    print('\nCADASTRO ARMAZENADO COM SUCESSO')
+    return True
 
 def listarAlunos():
     limparTela()
-
-    alunosCadastrados = carrgarAlunoCSV() # carrega os alunos do CSV
+    alunosCadastrados = carregarAlunosCSV()
 
     if not alunosCadastrados:
         print('NÃO HÁ NENHUM REGISTRO!')
         input('\nPRESSIONE "ENTER" PARA CONTINUAR...')
         return
     
-    print('\n' + '=' * 10  + ' REGISTRO DE ALUNOS ' + '=' * 10)
+    print('=' * 10 + ' REGISTROS DE ALUNOS ' + '=' * 10)
     for i, aluno in enumerate(alunosCadastrados):
-        print(f'\aALUNO #{i+1}:')
+        print(f'\nALUNO #{i+1}:')
         print('NOME: {}'.format(aluno['nome']))
         print('DATA DE NASCIMENTO: {}'.format(aluno['nascimento']))
         print('MATRÍCULA: {}'.format(aluno['matricula']))
-        print('E-MAIL: {}'.format(aluno['email']))    
+        print('E-MAIL: {}'.format(aluno['email']))     
         print('SENHA: {}'.format(aluno['senha']))
 
+    input('\nPRESSIONE "ENTER" PARA CONTINUAR...')
+
+def pesquisarPorNome():
+    limparTela()
+    alunos = carregarAlunosCSV()
+    if not alunos:
+        print('NÃO HÁ NENHUM REGISTRO PARA PESQUISAR!')
+        input('\nPRESSIONE "ENTER" PARA CONTINUAR...')
+        return
+
+    valorPesquisa = input('DIGITE O NOME OU PARTE DO NOME PARA PESQUISAR: ').strip().lower()
+    
+    alunosEncontrados = list(filter(lambda aluno: valorPesquisa in aluno['nome'].lower(), alunos))
+
+    if not alunosEncontrados:
+        print(f'\nNENHUM REGISTRO ENCONTRADO COM: "{valorPesquisa}".')
+    else:
+        print(f'\nREGISTRO(S) ENCONTRADO COM "{valorPesquisa}":')
+        for i, aluno in enumerate(alunosEncontrados):
+            print(f'\nALUNO #{i+1}:')
+            print('NOME: {}'.format(aluno['nome']))
+            print('DATA DE NASCIMENTO: {}'.format(aluno['nascimento']))
+            print('MATRÍCULA: {}'.format(aluno['matricula']))
+            print('E-MAIL: {}'.format(aluno['email']))     
+            print('SENHA: {}'.format(aluno['senha']))
+    
     input('\nPRESSIONE "ENTER" PARA CONTINUAR...')
 
 def executarSistema():
@@ -172,20 +182,22 @@ def executarSistema():
         if (opcao == 1): # cadastrar aluno
             dadosAlunos = capturarDados()
 
-            # validar dados
-            valido = validarDados(dadosAlunos)
+            valido, mensagem = validarDados(dadosAlunos) 
             if valido:
-                cadastrar(dadosAlunos)
+                if cadastrar(dadosAlunos):
+                    print(mensagem)
             else:
-                print('\nCADASTRO NÃO REALIZADO DEVIDO A ERROS NOS DADOS!')
+                print(mensagem)
 
             input('\nPRESSIONE "ENTER" PARA CONTINUAR...')
 
-        # listar os alunos
-        elif (opcao == 2):
+        elif (opcao == 2): # listar os alunos
             listarAlunos()
         
         elif (opcao == 3):
+            pesquisarPorNome()
+
+        elif (opcao == 4):
             limparTela()
             print('\nSAINDO DO PROGRAMA...')
             break
